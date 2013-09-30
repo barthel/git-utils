47692c477ae45af20b20c782b0813ca54a5f24e8
barthel <barthel@users.noreply.github.com>
2013-09-30 18:02:36 +0200

    comments added for git_editor.sh

--- git_editor.sh
+++ git_editor.sh
@@ -2,12 +2,20 @@
 
 . git_branch_name.sh
 
+# check and set EDITOR if needed
 if [ -z "$EDITOR" ]
     then
     EDITOR="/usr/bin/vi"
 fi
 
+# get comment char from git configuration
 GIT_COMMENT_CHAR=`git config --get core.commentchar`
+if [ -z "$GIT_COMMENT_CHAR" ]
+    then
+    GIT_COMMENT_CHAR="#"
+fi
+
+# replace comment char and branch name in template file
 sed -i "s/GIT_COMMENT_CHAR/$GIT_COMMENT_CHAR/g;s/BRANCH_NAME/$BRANCH_NAME/g" "$*"
 exec $EDITOR "$*"
 
