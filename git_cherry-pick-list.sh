1d7863a60a9b8da025c5af032d8433b255f54435
barthel <barthel@users.noreply.github.com>
2014-06-25 17:38:16 +0200

    ammend hash entry from list as commit comment for easier stash

--- git_cherry-pick-list.sh
+++ git_cherry-pick-list.sh
@@ -16,18 +16,20 @@ for commit in $(cat "${LIST_FILE}") ; do
 # format based on git alias: issue-cherry-pick-list
   hash=$(echo ${commit} | cut -d$' ' -f 2)
   echo -n "cherry picking ${hash} ... "
-  git cherry-pick -n "${hash}"
+  git cherry-pick "${hash}"
 
-  if [ $? -eq 0 ] ; then
-    if [ ${AUTO_RUN} -eq 1 ] ; then
+  if [[ $? -eq 0 ]]
+  then
+    if [[ ${AUTO_RUN} -eq 1 ]]
+    then
       echo "done."
     else
       read -p "Press [ENTER] key if you are sure and to continue ..."
-    fi¬
+    fi
   else
     echo "There are conflicts to resolve!"
     read -p "Press [ENTER] key if you have resolved the conflicts and to continue ..."
   fi
-  git commit -m"${commit}"
+  git commit --amend -m"${commit}"
 done
 
