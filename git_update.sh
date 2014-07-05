b05a56f2bef1f1b0fe35c8e73f8cdf379b803ff5
barthel <barthel@users.noreply.github.com>
2014-07-05 20:52:24 +0200

    update/clone repository based on repository name file

--- git_update.sh
+++ git_update.sh
@@ -2,43 +2,55 @@
 
 . git_branch_name.sh
 
+repo_list_file='.repositories'
+repo_names=('.')
+
 # check if the current directory contains git repository
-if [ -d ".git" ]
+if [ ! -d ".git" ]
   then
-    # repository name
-    REPO_NAMES="."
-  else
-    # repository names locate in directory
-    REPO_NAMES="`ls -A $DIR_NAME`"
+  if [ ! -f "$DIR_NAME/$repo_list_file" ]
+    then
+      # repository names locate in directory
+      repo_names=(`ls -A "${DIR_NAME}"`)
+    else
+      repo_names=(`cat "${repo_list_file}"`)
+  fi
 fi
 
 cd "$DIR_NAME"
 
 # actualize branches
-for f in $REPO_NAMES; do
-  if [ -d "$DIR_NAME/$f" ]
+counter=1
+size=${#repo_names[@]}
+for repo in "${repo_names[@]}" ; do
+  local_dir=${repo//[^a-zA-Z_\.]/_}
+  echo ''
+  if [ -d "$DIR_NAME/$local_dir/.git" ]
     then
-    echo "$f: ";
-    if [ -d "$DIR_NAME/$f/.git" ]
-        then
-        cd "$DIR_NAME/$f";
-        echo "update $f and switch to branch: $BRANCH_NAME"
-        git fetch --all --prune;
-        git checkout -B $BRANCH_NAME -t -f origin/$BRANCH_NAME;
-        git fetch;
-        git rebase origin/$BRANCH_NAME;
+      echo "[$counter/$size] update $repo: ";
+      cd "$DIR_NAME/$local_dir";
+      echo "update $repo and switch to branch: $BRANCH_NAME"
+      git fetch --all --prune;
+      git checkout -B $BRANCH_NAME -t -f origin/$BRANCH_NAME;
+      git fetch;
+      git rebase origin/$BRANCH_NAME;
     else
-        cd "$DIR_NAME";
-        echo "clone $f"
-        if [ "$f" == "$PATCH_LOCAL_NAME" ]
+      echo "clone: $repo to: $DIR_NAME/$local_dir"
+      cd "$DIR_NAME";
+      if [ -d "$DIR_NAME/$local_dir" ]
         then
-            git clone --branch $BRANCH_NAME ssh://git@git-server.icongmbh.de/$PATCH_REPO_NAME $PATCH_LOCAL_NAME
+        cd "$DIR_NAME/$local_dir"
+        local_dir='.'
+      fi
+      if [ "$repo" == "$PATCH_LOCAL_NAME" ]
+        then
+          git clone --branch $BRANCH_NAME ssh://git@git-server.icongmbh.de/$PATCH_REPO_NAME $PATCH_LOCAL_NAME
         else
-            git clone --branch $BRANCH_NAME ssh://git@git-server.icongmbh.de/$f $f
-        fi
-    fi
-    cd "$DIR_NAME";
+            git clone --branch $BRANCH_NAME ssh://git@git-server.icongmbh.de/$repo $local_dir
+      fi
   fi
+  cd "$DIR_NAME";
+  counter=$((counter + 1))
 done
 
 . git_patch.sh
