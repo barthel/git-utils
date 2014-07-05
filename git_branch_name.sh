08c11a61b4cf60ef3c996334ec796037b25cf55f
barthel <barthel@users.noreply.github.com>
2014-07-05 20:50:51 +0200

    branch file name moved into constant

--- git_branch_name.sh
+++ git_branch_name.sh
@@ -3,18 +3,20 @@
 # directory name
 export DIR_NAME="`pwd`"
 
+branch_name_file='.branch_name'
+
 if [ "" == "$BRANCH_NAME" ]
   then
   if ! git rev-parse --git-dir > /dev/null 2>&1;
   then
     # check file containing the branch name
-    if [ ! -f "$DIR_NAME/.branch_name" ]
+    if [ ! -f "$DIR_NAME/$branch_name_file" ]
     then
-        echo "file >.branch_name< or git repository is needed"
+        echo "file >$branch_name_file< or git repository is needed"
         exit 1
     fi
     # get branch name from file
-    export BRANCH_NAME="$(<$DIR_NAME/.branch_name)"
+    export BRANCH_NAME="$(<$DIR_NAME/$branch_name_file)"
   else
     export BRANCH_NAME="`git rev-parse --abbrev-ref HEAD`"
   fi
