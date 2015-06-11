48f271d668d4d8456e7a83056c8f5570ad4a544d
barthel <barthel@users.noreply.github.com>
2015-06-11 07:39:09 +0200

    Add commit message template and small documentation in git_editor.sh

--- git_editor.sh
+++ git_editor.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+#
+# This script is wrapping the commit message editor for git.
+# Replace the following placeholder:
+#  GIT_COMMENT_CHAR - with the configured comment char got by 'git config'
+#  BRANCH_NAME - with the actual branch name
+# 
+set -m
+#set -x
 
 . git_branch_name.sh
 
 # check and set EDITOR if needed
-if [ -z "$EDITOR" ]
-    then
-    EDITOR="/usr/bin/vi"
-fi
+[ -z "${EDITOR}" ] && EDITOR="/usr/bin/vi" || true
 
 # get comment char from git configuration
-GIT_COMMENT_CHAR=`git config --get core.commentchar`
-if [ -z "$GIT_COMMENT_CHAR" ]
-    then
-    GIT_COMMENT_CHAR="#"
-fi
+GIT_COMMENT_CHAR="$(git config --get core.commentchar)"
+[ -z "$GIT_COMMENT_CHAR" ] && GIT_COMMENT_CHAR="#" || true
 
 # replace comment char and branch name in template file
-sed -i "s/GIT_COMMENT_CHAR/$GIT_COMMENT_CHAR/g;s/BRANCH_NAME/$BRANCH_NAME/g" "$*"
-exec $EDITOR "$*"
+sed -i "s/GIT_COMMENT_CHAR/${GIT_COMMENT_CHAR}/g;s/BRANCH_NAME/${BRANCH_NAME}/g" "$*"
+exec ${EDITOR} "$*"
 
 exit $?
 
