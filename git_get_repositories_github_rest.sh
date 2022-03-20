8f6bf0ac58b2f680d11ad4e058a2ae8d52ea7542
barthel <barthel@users.noreply.github.com>
2022-03-20 09:27:26 +0100

    Enhance get Github repositories script to list private repositories too.

--- git_get_repositories_github_rest.sh
+++ git_get_repositories_github_rest.sh
@@ -8,7 +8,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-# Get all repositories for a user provided by a GitHub server via REST API.
+# Get all public and private repositories of my own or all (only) pubic repositories for a user provided by a GitHub server via REST API.
 #
 # Write the repository name and the ssh repository url to standard out:
 # <repo name><TAB><repo url>
@@ -25,17 +25,24 @@ curl_cmd="curl -s"
 
 [ -z "${verbose}" ] && verbose=0
 github_api_server="https://api.github.com"
+affiliation="owner"
 
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-h?v] [-s SERVER_URL] -u USER
+Usage: ${0##*/} [-h?v] [-a AFFILIATION] [-s SERVER_URL] [-t PAT | -u USER]
 
 Get all GIT repositories for a user provided by a GitHub server via REST API.
 
-    -h|-?         display this help and exit.
-    -s SERVER_URL GitHub API server URL (default: https://api.github.com).
-    -u USER       GitHub user ID.
-    -v            verbose mode. Can be used multiple times for increased verbosity.
+    -h|-?           display this help and exit.
+    -a AFFILIATION  Optional comma-separated list of values. Can include:
+                    * owner: Repositories that are owned by the authenticated user. (Default)
+                    * collaborator: Repositories that the user has been added to as a collaborator.
+                    * organization_member: Repositories that the user has access to through being a member of an organization.
+                      This includes every repository on every team that the user is on.
+    -s SERVER_URL   GitHub API server URL (default: https://api.github.com).
+    -t PAT          Personal Access Token to fetch your own public and private repositories.
+    -u USER         GitHub user ID to fetch the public repositories from.
+    -v              verbose mode. Can be used multiple times for increased verbosity.
 
 Example: ${0##*/} -s https://github.company.tld -u ${USER}
 EOF
@@ -46,15 +53,21 @@ EOF
 # @see: http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash#192266
 # @see: http://mywiki.wooledge.org/BashFAQ/035#getopts
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
-while getopts "p:s:u:vh?" opt;
+while getopts "a:p:s:t:u:vh?" opt;
 do
   case "$opt" in
     h|\?)
       show_help
       exit 0
     ;;
+    a)
+      affiliation="$OPTARG"
+    ;;
     s)
       github_api_server="$OPTARG"
+    ;;
+    t)
+      pat="$OPTARG"
     ;;
     u)
       user="$OPTARG"
@@ -67,14 +80,28 @@ done
 
 shift $((OPTIND-1))
 
-[ -z "${user}" ] && echo "GitHub user ID required." && show_help && exit 1
-
 # @see: https://docs.github.com/en/rest/overview/media-types
-curl_cmd+=' -H "Accept: application/vnd.github.v3+json"'
-# @see: https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user
-#   https://api.github.com/users/barthel/repos?per_page=100
-curl_cmd+=" -X GET ${github_api_server}/users/${user}/repos?per_page=100"
+http_headers='Accept: application/vnd.github.v3+json;'
 
-${curl_cmd} | jq -r '.[] | select(.archived == false) | "\(.name)\tssh://\(.ssh_url)"'
+if [ -n "${pat}" ]
+then
+  # get all private and public repositories of Github user provided by the Personal Access Token
+  # @see: https://docs.github.com/en/rest/reference/repos#list-repositories-for-the-authenticated-user
+  http_headers="Authorization: token ${pat}"
+  curl_cmd+=" -X GET ${github_api_server}/user/repos?visibility=all&archived=false&affiliation=${affiliation}"
+elif [ -n "${user}" ]
+then
+  # get (only) public repositories of passed Github User ID
+  # @see: https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user
+  #   https://api.github.com/users/barthel/repos?per_page=100
+  curl_cmd+=" -X GET ${github_api_server}/users/${user}/repos?per_page=100"
+  [ "${affiliation}" != "owner" ] && curl_cmd+="&type=all"
+else
+  echo "At least one Personal Access Token (PAT) or Github user ID is required."
+  show_help
+  exit 1
+fi
+
+${curl_cmd} -H "${http_headers}" | jq -r '.[] | select(.archived == false) | "\(.name)\tssh://\(.ssh_url)"'
 
 #
